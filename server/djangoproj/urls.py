f604fa83eec9dd4246108df30aba6b1b71bd6549
charzynm <charzynm@wp.pl>
2024-06-11 18:04:16 +0000

    Added logout

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-from djangoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +25,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    path(route='login', view=views.login_user, name='login'),
     path('login/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
