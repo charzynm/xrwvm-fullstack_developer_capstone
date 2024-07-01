af549a62eea6488a8758481e0000d6a452df3566
charzynm <charzynm@wp.pl>
2024-07-01 12:54:35 +0000

    Bug fixed

--- server/database/app.js
+++ server/database/app.js
@@ -84,7 +84,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
   try {
-    const documents = await Reviews.find({id: req.params.id});
+    const documents = await Dealerships.find({id: req.params.id});
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
